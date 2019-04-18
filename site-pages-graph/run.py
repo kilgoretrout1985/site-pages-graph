593aeafaed25fbae13cf3971cd13d458033fcd5a
kilgoretrout1985 <zemskyura@gmail.com>
2019-04-18 13:54:07 +0300

    Help added to run script.

--- site-pages-graph/run.py
+++ site-pages-graph/run.py
@@ -1,6 +1,11 @@
-from sys import argv
+import sys
+
 from lib.graph import Graph
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2 or sys.argv[1] == '--help':
+        print("Run this file with site root url like:")
+        print("python3 %s https://mysite.com/" % sys.argv[0])
+        sys.exit(0)
+    
