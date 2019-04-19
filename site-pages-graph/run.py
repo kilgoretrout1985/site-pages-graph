85eff8ccab0074b232a1f3e350b846084807dede
kilgoretrout1985 <zemskyura@gmail.com>
2019-04-19 15:43:58 +0300

    Basic 1 thread parsing in runner remade for queues.

--- site-pages-graph/run.py
+++ site-pages-graph/run.py
@@ -1,15 +1,10 @@
 import sys
-from urllib.request import Request, urlopen
+
+import url
+import requests
 
 from lib.graph import Graph
 from lib.link_helpers import find_links, normalize_links, filter_links
-
-
-def get_undone(todo: dict) -> str:
-    for key in todo:
-        if not todo[key]:  # if not done
-            return key
-    return None
 
 
 if __name__ == '__main__':
@@ -18,28 +13,55 @@ if __name__ == '__main__':
         print("python3 %s https://mysite.com/" % sys.argv[0])
         sys.exit(0)
     
-    graph = Graph({})  # start empty graph
-    url = str(url.parse( sys.argv[1] ).defrag().abspath())  # start url /
-    todo = { url: False }  # url : is_done
+    graph = Graph({})
+    start_url = str(url.parse( sys.argv[1] ).defrag().abspath())  # start url /
+    todo_urls = [(start_url, 0),]  # list of tuples (url, error_count)
+    done_urls = {}  # save info about processed urls here
+
     while True:
-        current_url = get_undone(todo)
-        if current_url is None:
-            break
+        try:
+            current_url, current_url_errors = todo_urls.pop(0)
+        except IndexError:
+            break  # all done
         
-        print(current_url)
-        html = urlopen(current_url, timeout=5)
-        links = find_links(html)
-        links = normalize_links(links, current_url)
-        links = filter_links(links, current_url)
+        if current_url in done_urls:
+            continue  # was already
         
-        # page with no links to other pages is still a node in a graph
+        try:
+            response = requests.get(current_url, timeout=5)
+            if response.status_code > 499:
+                response.raise_for_status()
+            
+            print("Received %s" % current_url)
+            done_urls[current_url] = {
+                'status': response.status_code,
+                'redirects_to': None,
+            }
+        except IOError as e:
+            current_url_errors += 1
+            if current_url_errors < 5:
+                # retry later
+                todo_urls.append((current_url, current_url_errors))
+            else:
+                # write error info
+                done_urls[current_url] = {
+                    'status': response.status_code,
+                    'redirects_to': None,
+                }
+            print("Error at %s: %s" % (current_url, str(e)))
+            continue
+    
+        # every page (even with no links to others) is a node in a graph
         graph.add_node(current_url)
-        todo[current_url] = True  # this link is done
+
+        links = find_links(response.text)
+        links = normalize_links(links, current_url)
+        links = filter_links(links, current_url)        
 
         for link in links:
             # add newly discovered for the job
-            if link not in todo:
-                todo[link] = False
+            if link not in done_urls:
+                todo_urls.append((link, 0,))
             # add to graph
             graph.add_edge(current_url, link)
             
