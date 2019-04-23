d853d093db3e3c1d5cec1289a1cc4cc1df7f78c3
kilgoretrout1985 <zemskyura@gmail.com>
2019-04-23 16:06:14 +0300

    Runner script is almost 100% workable - parses urls, dumps graph, saves data as csv.

--- site-pages-graph/run.py
+++ site-pages-graph/run.py
@@ -1,4 +1,6 @@
 import sys
+import os.path
+import csv
 
 import url
 import requests
@@ -12,7 +14,7 @@ if __name__ == '__main__':
         print("Run this file with site root url like:")
         print("python3 %s https://mysite.com/" % sys.argv[0])
         sys.exit(0)
-    
+
     graph = Graph({})
     start_url = str(url.parse( sys.argv[1] ).defrag().abspath())  # start url /
     todo_urls = [(start_url, 0),]  # list of tuples (url, error_count)
@@ -64,5 +66,52 @@ if __name__ == '__main__':
                 todo_urls.append((link, 0,))
             # add to graph
             graph.add_edge(current_url, link)
-            
-    print(graph)
+
+    # prepare the results and write them
+    host = url.parse(start_url).host
+    out_d = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output')
+
+    # save graph
+    graph_file = os.path.join(out_d, host+'.pickle')
+    graph.save_to_file(graph_file)
+    print("Graph saved to %s" % graph_file)
+
+    # compute clicks from graph one time for csv and sqlite
+    print("Working with graph. This can be slow. Wait for a while.")
+    c = 0
+    for url in done_urls:
+        # clicks to this page from homepage
+        done_urls[url]['clicks'] = len(graph.find_shortest_path(start_url, url)) - 1
+        # internal link to this page from other pages
+        internal_links = 0
+        for node in graph.g:
+            if node != url and url in graph.g[node]:
+                internal_links += 1
+        done_urls[url]['internal_links'] = internal_links
+        # print to show work is going
+        c += 1
+        if (c % 10) == 0:
+            print(".", sep='', end='', flush=True)
+    
+    # write csv
+    csv_file = os.path.join(out_d, host+'.csv')
+    with open(csv_file, mode='w') as csv_f:
+        csv_writer = csv.writer(csv_f)
+        csv_writer.writerow([
+            'url',
+            'status',
+            'clicks from /',
+            'internal links to this url',
+            'url redirects to'
+        ])
+        for url in done_urls:
+            csv_writer.writerow([
+                url,
+                done_urls[url]['status'],
+                done_urls[url]['clicks'],
+                done_urls[url]['internal_links'],
+                ''
+            ])
+    print("Saved csv table to %s" % csv_file)
+
+
