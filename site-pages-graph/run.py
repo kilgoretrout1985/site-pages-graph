e265ccd14b28eebf475f8d417fbc495c2cf4a433
kilgoretrout1985 <zemskyura@gmail.com>
2019-04-24 17:42:58 +0300

    multithread version

--- site-pages-graph/run.py
+++ site-pages-graph/run.py
@@ -1,6 +1,7 @@
 import sys
 import os.path
 import csv
+import concurrent.futures
 
 import url
 import requests
@@ -8,6 +9,17 @@ import requests
 from lib.graph import Graph
 from lib.link_helpers import find_links, normalize_links, filter_links, \
                              is_internal_link
+
+
+MAX_THREADS = 20
+
+
+def get_url(url: str) -> requests.Response:
+    try:
+        response = requests.get(url, timeout=5)
+        return (url, response, None)
+    except Exception as e:
+        return (url, None, e)
 
 
 if __name__ == '__main__':
@@ -18,69 +30,77 @@ if __name__ == '__main__':
 
     graph = Graph({})
     start_url = str(url.parse( sys.argv[1] ).defrag().abspath())  # start url /
-    todo_urls = [(start_url, 0),]  # list of tuples (url, error_count)
+    todo_urls = { start_url: 0, }  # dict { url: error_count }
     done_urls = {}  # save info about processed urls here
 
     while True:
-        try:
-            current_url, current_url_errors = todo_urls.pop(0)
-        except IndexError:
+        urls = [x for x in todo_urls.keys() if x not in done_urls]
+        if not urls:
             break  # all done
-        
-        if current_url in done_urls:
-            continue  # was already
-        
-        try:
-            response = requests.get(current_url, timeout=5)
-            
-            if response.status_code > 499:
-                response.raise_for_status()
-            
-            # check for redirect
-            redirect_to = None
-            redirect_status = None
-            if response.history \
-            and response.history[0].status_code in (301, 302):
-                redirect_to = response.url
-                redirect_status = response.history[0].status_code
-            
-            done_urls[current_url] = {
-                'status': redirect_status if redirect_status is not None \
-                                else response.status_code,
-                'redirect_to': redirect_to,
-            }
-            print("Received %s" % current_url)
-        except IOError as e:
-            current_url_errors += 1
-            if current_url_errors < 5:
-                # retry later
-                todo_urls.append((current_url, current_url_errors))
-            else:
-                # write error info
+
+        threads_count = min(MAX_THREADS, len(urls))
+        with concurrent.futures.ThreadPoolExecutor(threads_count) as executor:
+            results = list(executor.map(get_url, urls))
+
+        for current_url, response, exc in results:
+            try:
+                if exc is not None:
+                    raise exc
+                
+                if response.status_code > 499:
+                    response.raise_for_status()
+                
+                # check for redirect
+                redirect_to = None
+                redirect_status = None
+                if response.history \
+                and response.history[0].status_code in (301, 302):
+                    redirect_to = response.url
+                    redirect_status = response.history[0].status_code
+                
+                # write data
                 done_urls[current_url] = {
-                    'status': response.status_code,
-                    'redirect_to': None,
+                    'status': redirect_status if redirect_status is not None \
+                                    else response.status_code,
+                    'redirect_to': redirect_to,
                 }
-            print("Error at %s: %s" % (current_url, str(e)))
-            continue
-    
-        # every page (even with no links to others) is a node in a graph
-        graph.add_node(current_url)
+                # clean up on successful retrieve
+                del todo_urls[current_url]
+                print("Processed %s" % current_url)
+            except Exception as e:
+                # value of a dict is number of tries to get the url
+                todo_urls[current_url] += 1
+                if todo_urls[current_url] > 5:  # stop trying
+                    # write error info
+                    try:
+                        status = response.status_code
+                    except AttributeError:  # response can be None
+                        status = 0
+                    done_urls[current_url] = {
+                        'status': status,
+                        'redirect_to': None,
+                    }
+                    del todo_urls[current_url]
+                print("Error at %s: %s" % (current_url, str(e)))
+                continue
+        
+            # every page (even with no links to others) is a node in a graph
+            graph.add_node(current_url)
 
-        links = find_links(response.text)
-        links = normalize_links(links, current_url)
-        links = filter_links(links, current_url)
-        # add redirect to crawl and count it as a linked page from current page
-        if redirect_to is not None \
-        and is_internal_link(redirect_to, current_url):
-            links.append(redirect_to)
+            links = find_links(response.text)
+            links = normalize_links(links, current_url)
+            links = filter_links(links, current_url)
+            # add redirect to crawl and count it as a linked page from current page
+            if redirect_to is not None \
+            and is_internal_link(redirect_to, current_url):
+                links.append(redirect_to)
 
-        for link in links:
-            # add newly discovered for the job
-            if link not in done_urls:
-                todo_urls.append((link, 0,))
-            # add to graph
-            graph.add_edge(current_url, link)
+            for link in links:
+                # add newly discovered for the job
+                if link not in done_urls:
+                    todo_urls[link] = 0
+                # add to graph
+                graph.add_edge(current_url, link)
 
 
     # prepare the results and write them
